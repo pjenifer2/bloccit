150d86ae83062f17ce9c635a98a56d738455d31d
Patrick <pjuofa95@gmail.com>
2015-12-23 22:04:16 -0700

    assignment 30 seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,37 @@
 include RandomData
 
 #create posts
-50.times do
-  Post.create!(
-  title: RandomData.random_sentence,
-  body: RandomData.random_paragraph
-  )
-end
+#50.times do
+#  Post.create!(
+#  title: RandomData.random_sentence,
+#  body: RandomData.random_paragraph
+#  )
+#end
 
-posts = Post.all
+#posts = Post.all
 
-100.times do
-  Comment.create!(
-  post: posts.sample,
-  body: RandomData.random_paragraph
-  )
-end
+#100.times do
+#  Comment.create!(
+#  post: posts.sample,
+#  body: RandomData.random_paragraph
+#  )
+#end
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
+Post.find_or_create_by(
+title: "Star Wars the Force Awakens",
+body: "Star Wars returns with it's seventh installment about good vs evil in a galaxy far far away"
+)
+
+Comment.find_or_create_by(
+body: "A great movie"
+)
+
+#exercise does not state that the comment needs to be associated with the post.
+#so I'm not going to do that... so ... this leads to more questions
+#I image this exercise is for creating test data to help you code .. makes sense
+#but then how do you associate the comment with the post ... this would be crtical
+#in my opinion.
