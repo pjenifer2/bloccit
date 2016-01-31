a99f95f3ca1b857a64f36a0e9e4dc1f1e22831d2
Patrick <pjuofa95@gmail.com>
2016-01-31 10:53:12 -0700

    half way checkpoint 44

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130182403) do
+ActiveRecord::Schema.define(version: 20160131172203) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -60,6 +60,16 @@ ActiveRecord::Schema.define(version: 20160130182403) do
 
   add_index "commetings", ["comment_id"], name: "index_commetings_on_comment_id"
   add_index "commetings", ["commentable_type", "commentable_id"], name: "index_commetings_on_commentable_type_and_commentable_id"
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "favorites", ["post_id"], name: "index_favorites_on_post_id"
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
 
   create_table "labelings", force: :cascade do |t|
     t.integer  "label_id"
